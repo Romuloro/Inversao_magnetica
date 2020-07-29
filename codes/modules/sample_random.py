d10dfba2cfff5d061b5c3a5423b7b58edaa37c30
RÔMULO RODRIGUES DE OLIVEIRA <68561301+Romuloro@users.noreply.github.com>
2020-07-29 13:59:33 -0300

    Docstrings
    Documentação e novo formato de entrada

--- codes/modules/sample_random.py
+++ codes/modules/sample_random.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def f_difference (dado_referencia, dado_calculado):
+    """
+    Função com o objetivo de calcular o valor da função diferença entre os dados de referência para os dados calculados.
+
+    :param dado_referencia: O dado de referência.
+    :param dado_calculado: O dado calculado que será comparado.
+    :return: Valor da função diferença.
+    """
+
     std = np.std(dado_referencia)
     dif = (dado_referencia - dado_calculado)**2 / (std**2)
     rms = np.sum(dif) / len(dif)
@@ -8,14 +16,26 @@ def f_difference (dado_referencia, dado_calculado):
     return rms
 
 
-def sample_random_coordinated(dicionario):
-    xmax = dicionario.get('xmax')
-    xmin = dicionario.get('xmin')
-    ymax = dicionario.get('ymax')
-    ymin = dicionario.get('ymin')
-    zlim = dicionario.get('zlim')
-    n = dicionario.get('n')
-    
+def sample_random_coordinated(xmax, xmin, ymax, ymin, zlim, z_min, n):
+    """
+    Função com o objetivo de criar de forma randomica as coordenadas para n corpos.
+
+    As entradas da função é feita da forma clássica ou através de um dicionário que é descompactado.
+    O dicinário deve conter as chaves nomeadas de forma identica aos parâmetros de entrada da função.
+    Exemplo de entrada: sample_random_coordinated(**dicionario).
+
+    :param dicionario: xmax - O valor máximo da coordenada X.
+                       ymax - O valor máximo da coordenada Y.
+                       zlim - O valor máximo da coordenada Z.
+                       xmin - O valor minímo da coordenada X.
+                       ymin - O valor minímo da coordenada Y.
+                       z_min - O valor minímo da coordenada Z.
+                       n - número de bolinhas desejadas.
+    :return: resultadox - Lista com o resultado final para das coordenadas no eixo X.
+             resultadoy - Lista com o resultado final para das coordenadas no eixo Y.
+             resultadoz - Lista com o final para das coordenadas no eixo Z.
+    """
+
     resultadox=[]
     resultadoy=[]
     resultadoz=[]
@@ -30,15 +50,28 @@ def sample_random_coordinated(dicionario):
 #---------------------------------------------------------------------------------------------------------------------#        
     return resultadox, resultadoy, resultadoz
 
-def sample_random_mag(dicionario, homogeneo):
-    inclmax = mag_bounds.get('inclmax')
-    inclmin = mag_bounds.get('inclmin')
-    declmax = mag_bounds.get('declmax')
-    declmin = mag_bounds.get('declmin')
-    magmax = mag_bounds.get('mag_max')
-    magmin = mag_bounds.get('mag_min')
-    n = mag_bounds.get('n')
-#---------------------------------------------------------------------------------------------------------------------#
+def sample_random_mag(inclmax, inclmin, declmax, declmin, magmax, magmin, n, homogeneo=False):
+    """
+    Função com o objetivo de criar de forma randomica as propriedades magnéticas para n corpos.
+
+    As entradas da função é feita da forma clássica ou através de um dicionário que é descompactado.
+    O dicinário deve conter as chaves nomeadas de forma identica aos parâmetros de entrada da função.
+    Exemplo de entrada: sample_random_mag(**dicionario, homogeneo).
+
+    :param dicionario: inclmax - Valor máximo da inclianção magnética.
+                       inclmin = Valor mínimo da inclianção magnética.
+                       declmax = Valor máximo da inclianção magnética.
+                       declmin = Valor mínimo da declianção magnética.
+                       magmax = Valor máximo da magnetização.
+                       magmin = Valor mínimo da magnetização.
+                       n - número de bolinhas desejadas.
+    :param homogeneo: True para valores de magnetização iguais para as n bolinhas.
+                      False é a opção default, onde os valores de magnetização é criada de forma randominca.
+    :return: incl - Lista com os valores de inclinação magnética.
+             decl - Lista com os valores de declinação magnética.
+             mag - Lista com os valores de magnetização.
+    """
+
     incl=[]
     decl=[]
     mag=[]
@@ -64,24 +97,30 @@ def sample_random_mag(dicionario, homogeneo):
     
     return incl, decl, mag
 
-def tfa_n_dots(dicionario1, dicionario2):
-    #Dicionário com os valores de mag
-    incl = dicionario1.get('incl')
-    decl = dicionario1.get('decl')
-    mag = dicionario1.get('mag')
-    n = dicionario1.get('n')
-#---------------------------------------------------------------------------------------------------------------------#
-    #Dicionário com os valores de coordenadas
-    Xref = dicionario2.get('Xref')
-    Yref = dicionario2.get('Yref')
-    Zref = dicionario2.get('Zref')
-    I = dicionario2.get('I')
-    D = dicionario2.get('D')
-    coodX = dicionario2.get('coodX')
-    coodY = dicionario2.get('coodY')
-    coodZ = dicionario2.get('coodZ')
-    raio = dicionario2.get('raio')
-#---------------------------------------------------------------------------------------------------------------------#  
+def tfa_n_dots(incl, decl, mag, n, Xref, Yref, Zref, I, D, coodX, coodY, coodZ, raio):
+    """
+    Função com o objetivo calcular a anomalia magnética de n bolinhas.
+
+    As entradas da função é feita da forma clássica ou através de um dicionário que é descompactado.
+    O dicinário deve conter as chaves nomeadas de forma identica aos parâmetros de entrada da função.
+    Exemplo de entrada: tfa_n_dots(**dicionario).
+
+    :param dicionario: incl - Lista com os valores de inclinação magnética.
+                       decl - Lista com os valores de declinação magnética.
+                       mag - Lista com os valores de magnetização.
+                       n - número de bolinhas desejadas.
+                       Xref - Matrix com as coordenadas em X.
+                       Yref - Matrix com as coordenadas em Y.
+                       Zref - Matrix com as coordenadas em Z.
+                       I - valor de inclinação regional.
+                       D - valor de declinação regional.
+                       coodX - Lista com os valores de coordenada de cada bolinha no eixo X.
+                       coodY - Lista com os valores de coordenada de cada bolinha no eixo Y.
+                       coodZ - Lista com os valores de coordenada de cada bolinha no eixo Z.
+                       raio - O raio de cada bolinha.
+    :return: Uma matrix com os valores de anomália magnética para cada ponto do local estudado.
+    """
+
     sphere1 = []
     n = n
     for i in range(n):
